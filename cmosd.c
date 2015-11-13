eb0d80f414bd00d66b8987ba24d71503af03de2a
Oblivia Simplex <lucca.fraser@gmail.com>
2015-11-13 16:55:10 +0000

    Added cmos-reset functionality.

--- cmosd.c
+++ cmosd.c
@@ -13,6 +13,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CMOS_ADDR 0x70
+#define CMOS_DATA 0x71
+#define PSWD_INDEX 0x38
+#define CHKSUM_INDEX 0x2E
+#define PSWD_LEN 6
+
+
 int crack(char *cracked, char *enc_password, char *dicpath);
 
 int main(int argc, char **argv){
@@ -27,11 +34,18 @@ int main(int argc, char **argv){
   
 
   
-  if (ioperm(0x70, 2, 1)){ // ask permission (set to 1) for ports 0x70, 0x71
+  
+  if (ioperm(CMOS_ADDR, 2, 1)){ // ask permission (set to 1) for ports 0x70, 0x71
     perror("ioperm");
     exit (1);
+ }
+/*
+  if (0 != iopl(3)){ // try to obtain highest IO priv level
+    fprintf(stderr, "FATAL ERROR: UNABLE TO OBTAIN HIGHEST IOPL\n");
+    exit(EXIT_FAILURE);
   }
-
+  
+*/
   printf( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"
           "DUMPING BIOS PARAMETERS FROM CMOS\n"
           "This will take a moment...\n"
@@ -41,16 +55,16 @@ int main(int argc, char **argv){
   for (i = 0; i < 0x10; i++){
     if (i == 8)
       printf(" ");
-    printf("%2.2x ",i);
+    printf("%2.1x ",i);
   }
   printf("\n     +");
   for (i = 0; i < 49; i++)
     printf("-");
   printf("\n0x%2.2x | ",0);
   for (i = 0; i < 0x5C; i++){
-    outb(i, 0x70); // write to port 0x70
-    usleep(100000);
-    *(buffer + i) = inb(0x71);
+    outb(i, CMOS_ADDR);  // pass address to CMOS address register (port 0x70)
+    usleep(100000);      // give CMOS time to update data register
+    *(buffer + i) = inb(CMOS_DATA); // read from CMOS data register (port 0x70)
     printf("%2.2x ", *(buffer + i));
     if ((i+1) % 16 == 0){
       if ((i+1) == 0x40)
@@ -61,10 +75,23 @@ int main(int argc, char **argv){
   }
   puts("");
 
-  memcpy(password, (buffer+0x38), 6);
+  memcpy(password, (buffer + PSWD_INDEX), 6);
 
-  printf("\nENCRYPTED PASSWORD @ 0x38-0x44: ");
-  for (i = 0; i < 7; i++)
+  char ans;
+  printf("CMOS CHECKSUM @ 0x%2.2x: 0x%2.2x\n", CHKSUM_INDEX, *(buffer + CHKSUM_INDEX));
+  printf("RESET (y/N)? ");
+  scanf("%c",&ans);
+  if (ans == 'Y' || ans == 'y'){
+    outb(CHKSUM_INDEX, CMOS_ADDR);
+    usleep(100000);
+    outb(~(*(buffer + CHKSUM_INDEX)), CMOS_DATA);
+    usleep(100000);
+    printf("*** CHECKSUM INVERTED! CMOS[0x%2.2x] = 0x%2.2x ***\n", CHKSUM_INDEX, inb(CMOS_DATA));
+  }
+  
+      
+  printf("\nENCRYPTED PASSWORD @ 0x%2.2x-0x%2.2x: ", PSWD_INDEX, PSWD_INDEX + PSWD_LEN);
+  for (i = 0; i < 6; i++)
     printf("%2.2x ",*(password+i));
   printf("\n");
   
@@ -95,6 +122,7 @@ int crack(char *cracked, char *enc, char *dicpath){
   FILE *fd;
   if ((fd = fopen(dicpath, "r")) == NULL){
     fprintf(stderr, "FAILURE TO OPEN %s\nFATAL\n",dicpath);
+    exit(EXIT_FAILURE);
   }
   // basic idea: brute dictionary attack. loop through the dictionary
   // file, compare the encrypted version of the dictionary word with
